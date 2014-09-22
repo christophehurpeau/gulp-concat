35c7a7bfaa46290af448ab1d63edcb7eb5d36e13
Christophe Hurpeau <christophe@hurpeau.com>
2014-09-22 15:15:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,57 +5,72 @@ var PluginError = gutil.PluginError;
 var File = gutil.File;
 var Buffer = require('buffer').Buffer;
 var SourceMapConcatenator = require('sourcemap-concat').SourceMapConcatenator;
+
 module.exports = function(fileName, opt) {
-  if (!fileName) throw new PluginError('gulp-concat', 'Missing fileName option for gulp-concat');
-  if (!opt) opt = {};
-  // to preserve existing |undefined| behaviour and to introduce |newLine: ""| for binaries
-  if (typeof opt.newLine !== 'string') opt.newLine = gutil.linefeed;
-
-  var buffer = [];
-  var firstFile = null;
-  var newLineBuffer = opt.newLine ? new Buffer(opt.newLine) : null;
-
-  var sourceMapConcatenator;
-
-  function bufferContents(file) {
-    if (file.isNull()) return; // ignore
-    if (file.isStream()) return this.emit('error', new PluginError('gulp-concat',  'Streaming not supported'));
-
-    if (firstFile && newLineBuffer) buffer.push(newLineBuffer);
-    if (!firstFile) {
-      firstFile = file;
-      if (file.sourceMap) {
-        sourceMapConcatenator = new SourceMapConcatenator({ file: fileName });
-      }
+    if (!fileName) {
+        throw new PluginError('gulp-concat', 'Missing fileName option for gulp-concat');
     }
-    var fileContents = file.contents;
-    buffer.push(fileContents);
-
-    if (sourceMapConcatenator) {
-      sourceMapConcatenator.add(file.relative, fileContents.toString(), file.sourceMap);
+    if (!opt) {
+        opt = {};
     }
-  }
-
-  function endStream() {
-    if (buffer.length === 0) return this.emit('end');
-
-    var joinedContents = Buffer.concat(buffer);
-
-    var joinedPath = path.join(firstFile.base, fileName);
-
-    var joinedFile = new File({
-      cwd: firstFile.cwd,
-      base: firstFile.base,
-      path: joinedPath,
-      contents: joinedContents
-    });
-    if (sourceMapConcatenator) {
-      joinedFile.sourceMap = JSON.parse(sourceMapConcatenator.sourceMap.toString());
+    // to preserve existing |undefined| behaviour and to introduce |newLine: ""| for binaries
+    if (typeof opt.newLine !== 'string') {
+        opt.newLine = gutil.linefeed;
     }
 
-    this.emit('data', joinedFile);
-    this.emit('end');
-  }
+    var buffer = [];
+    var firstFile = null;
+    var newLineBuffer = opt.newLine ? new Buffer(opt.newLine) : null;
 
-  return through(bufferContents, endStream);
+    var sourceMapConcatenator;
+
+    function bufferContents(file) {
+        if (file.isNull()) {
+            return; // ignore
+        }
+        if (file.isStream()) {
+            return this.emit('error', new PluginError('gulp-concat', 'Streaming not supported'));
+        }
+
+        if (firstFile && newLineBuffer) {
+            buffer.push(newLineBuffer);
+        }
+        if (!firstFile) {
+            firstFile = file;
+            if (file.sourceMap) {
+                sourceMapConcatenator = new SourceMapConcatenator({ file: fileName });
+            }
+        }
+        var fileContents = file.contents;
+        buffer.push(fileContents);
+
+        if (sourceMapConcatenator) {
+            sourceMapConcatenator.add(file.relative, fileContents.toString(), file.sourceMap);
+        }
+    }
+
+    function endStream() {
+        if (buffer.length === 0) {
+            return this.emit('end');
+        }
+
+        var joinedContents = Buffer.concat(buffer);
+
+        var joinedPath = path.join(firstFile.base, fileName);
+
+        var joinedFile = new File({
+            cwd: firstFile.cwd,
+            base: firstFile.base,
+            path: joinedPath,
+            contents: joinedContents
+        });
+        if (sourceMapConcatenator) {
+            joinedFile.sourceMap = JSON.parse(sourceMapConcatenator.sourceMap.toString());
+        }
+
+        this.emit('data', joinedFile);
+        this.emit('end');
+    }
+
+    return through(bufferContents, endStream);
 };
